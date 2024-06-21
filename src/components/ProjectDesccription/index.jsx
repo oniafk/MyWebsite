e4d056c17a8734893568ba101b5cde89925ea2c1
Onimae <manufelipearias@gmail.com>
2024-06-21 18:41:36 +1000

    changed width to project image

--- src/components/ProjectDesccription/index.jsx
+++ src/components/ProjectDesccription/index.jsx
@@ -52,7 +52,7 @@ function ProjectDescription() {
               </div>
             </div>
           </div>
-          <div className="w-2/5 h-full pt-10 pb-10 border-t-slate-800 border-b-slate-800 border-t-2 border-b-2 max-h-fit max-w-md">
+          <div className="w-1/2 h-full pt-10 pb-10 border-t-slate-800 border-b-slate-800 border-t-2 border-b-2 max-h-fit max-w-md">
             <img
               src={context.currentProject.img}
               alt={context.currentProject.title}
