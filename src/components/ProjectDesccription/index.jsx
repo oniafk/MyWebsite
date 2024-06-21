cd972ca88d8ec6e9fae6ad3694eac75a6ce2d612
Onimae <manufelipearias@gmail.com>
2024-06-21 18:23:07 +1000

    updated import React

--- src/components/ProjectDesccription/index.jsx
+++ src/components/ProjectDesccription/index.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from "react";
+import React, { useContext } from "react";
 
 import { FilterContext } from "../../context";
 import { CloseModalButton } from "../CloseModalButton";
